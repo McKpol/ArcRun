fb1d774dd1fbf4b44edddc1450c63b9c71aaf890
McKpol <0809spinespine@gmail.com>
2024-01-22 19:19:16 +0100

    Added working search bar. A little style change.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,31 +1,37 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{fs::{self, OpenOptions}, io::Write};
+use std::{fs::{self, OpenOptions, File}, io::{Write, Read}};
 use walkdir::WalkDir;
 use whoami;
+use std::path::Path;
+
 
 fn cache_programs<>() -> Result<(), Box<dyn std::error::Error>> {
-
   // Data
   let user_name = whoami::username();
   let program_path = format!("C:/Users/{}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/", user_name);
   let path_file = format!("C:/Users/{}/AppData/Roaming/arcrun/", user_name);
   let chpro_file = format!("{}/chpro", path_file);
+  let chprocut_file = format!("{}/chprocut", path_file);
   
   // Creating dir and file
   fs::create_dir_all(path_file)?;
   let mut file = OpenOptions::new().read(true).write(true).create(true).append(true).open(chpro_file.clone())?;
+  let mut filecut = OpenOptions::new().read(true).write(true).create(true).append(true).open(chprocut_file.clone())?;
 
   // Empting a file
   fs::write(chpro_file, "")?;
+  fs::write(chprocut_file.clone(), "")?;
 
-  // Search folder, subfolder and files. Then Write to the file chpro_file. 
+  // Search folder, subfolder and files. Then Write to the file chpro_file, chprocut_file. 
   for programlist in WalkDir::new(program_path).into_iter().filter_map(|file| file.ok()) {
-    
-    if programlist.metadata().unwrap().is_file() {
-    let programlist = format!("{}", programlist.path().display());
-    writeln!(file, "{}", programlist)?;
+    if programlist.metadata().unwrap().is_file() && programlist.path().extension().unwrap().to_str().unwrap() != "ini" {
+      let programlist = format!("{}", programlist.path().display());
+      writeln!(file, "{}", programlist)?;
+
+      let path = Path::new(&programlist).file_stem().unwrap().to_str().unwrap();
+      writeln!(filecut, "{}", path)?;
     }
   }
   
@@ -33,12 +39,43 @@ fn cache_programs<>() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn main() {
-
+  println!("Script started");
+  
   match cache_programs(){
     Ok(_) => println!("Cashed program list"),
     Err(_) => println!("Cannot cached program list")
   }
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![search])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn search(search: String) -> String {
+  let user_name = whoami::username();
+  let path_file = format!("C:/Users/{}/AppData/Roaming/arcrun/", user_name);
+  let chprocut_file = format!("{}/chprocut", path_file);
+  
+  let mut filecut_ = File::open(chprocut_file).unwrap();
+  let mut buf = String::new();
+  let mut nline = 0;
+  filecut_.read_to_string(&mut buf).unwrap();
+  
+  for line in buf.lines(){
+    if line.to_lowercase().contains(&search){
+      println!("{} {}", search, nline);
+      break;
+    }
+    nline += 1;
+  }
+
+  let mut prime = "";
+  if nline != buf.lines().count() && nline != 0{
+    prime = buf.lines().nth(nline).unwrap();
+  }
+  println!("{}", prime);
+
+  prime.into()
+
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,30 +1,86 @@
 import { component$, useVisibleTask$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Image } from '@unpic/qwik';
-import { appWindow } from "@tauri-apps/api/window";
+import { appWindow, LogicalPosition, LogicalSize } from "@tauri-apps/api/window";
+import { invoke } from "@tauri-apps/api";
 
 export default component$(() => {
   useVisibleTask$(async () => {
-    appWindow.setFocus();
-    await appWindow.onFocusChanged(async () => {
-      if (!(await appWindow.isFocused())){
-        appWindow.close();
-      }
-  })
+  await appWindow.setSize(new LogicalSize(825, 90));
+  const result = document.getElementById("result")!;
+  const x = window.screen.width / 2 - window.innerWidth / 2;
+  const inputElement = document.getElementById("input");
+  let y = window.screen.height / 3.8 - window.innerHeight /2;
+  await appWindow.setPosition(new LogicalPosition(x, y));
+
+  //   appWindow.setFocus();
+  //   await appWindow.onFocusChanged(async () => {
+  //     if (!(await appWindow.isFocused())){
+  //       appWindow.close();
+  //     }
+  // })
+
+  inputElement?.addEventListener("input", async function() {
+    await appWindow.setPosition(new LogicalPosition(x, y));
+    const message: string = await invoke("search", {
+    search: document.getElementsByTagName("input")[0].value.toLowerCase(),
+  });
+    
+    const program = document.getElementById("program")!;
+    console.log(message); 
+
+    if (message == ""){
+      result.style.display = "none";
+      await appWindow.setSize(new LogicalSize(825, 90));
+      y = window.screen.height / 3.8 /* 2 */ - window.innerHeight / 2;
+    } else {
+      result.style.display = "block";
+      await appWindow.setSize(new LogicalSize(825, 600));
+      y = window.screen.height / 2 /* 2 */ - window.innerHeight / 2;
+      program.textContent = message; // Type 'unknown' is not assignable to type 'string | null'.
+    }
+  });
 });
+
   return (
     <>
-      <div class='w-full h-full bg-gray-200 absolute rounded-[20px] text-gray border-2 border-gray-400'>
+    <div id='check' class='invisible text-[0px]' />
+      <div class='bg-gray-200 absolute rounded-[20px] text-gray border-2 border-gray-300 h-[90px] w-[800px] left-1/2 -translate-x-1/2'>
         <div class='flex w-full h-full'>
           <div class='ml-2 h-[98%] w-20 '>
             <Image width={47} height={47} class="opacity-100 top-1/2 -translate-y-1/2 relative left-1/2 -translate-x-1/2" src="/logosearch.png" />
           </div>
           <div class='w-full'>
-            <input placeholder="Search with ArcRun" class='font-roboto text-[40px] bg-transparent text-black px-2 h-full border-transparent w-[100%] relative top-1/2 -translate-y-1/2 focus:outline-none' />
+            <input id="input" placeholder="Search with ArcRun" class='font-roboto text-[40px] bg-transparent text-black px-2 h-full border-transparent w-[100%] relative top-1/2 -translate-y-1/2 focus:outline-none' />
           </div>
             <div class='bg-gradient-to-r from-transparent via-gray-200/70 to-gray-200 absolute w-12 h-full right-0 mr-5' />
             <div class='w-4' />
           </div>
+      </div>
+      
+      <div id='result' class='hidden bg-white/0 h-5/6 w-full absolute bottom-0 roundedd-[20px]'>
+      {/* div jako program */}
+      <div class='mb-1 relative shadow-lg left-1/2 -translate-x-1/2 hover:w-full transition-all duration-200 cursor-pointer rounded-[10px] w-[795px] h-[75px] bg-gray-200 flex overflow-hidden 
+        before:duration-100 before:left-1/2 before:-translate-x-1/2 before:bg-[#2f82f7]/0 before:hover:bg-[#2f82f7] before:w-[96%] before:rounded-[9px] before:hover:w-full before:h-full before:delay-75 before:transition-all before:absolute'>
+          <div class='h-[98%] w-14'>
+            <Image width={38} height={38} class="ml-1 relative left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" src="/program.png" />
+          </div>
+          <div class='w-[93%]'>
+            <div id='program' class='relative top-1/2 -translate-y-1/2 px-2 pt-[0px] font-roboto font-normal text-[28px] truncate'></div>
+          </div>
+
+        </div>
+        {/* div jako folder */}
+        <div class='relative shadow-lg left-1/2 -translate-x-1/2 hover:w-full transition-all duration-200 cursor-pointer rounded-[10px] w-[795px] h-[50px] bg-gray-200 flex overflow-hidden 
+        before:duration-100 before:left-1/2 before:-translate-x-1/2 before:bg-[#2f82f7]/0 before:hover:bg-[#2f82f7] before:w-[96%] before:rounded-[9px] before:hover:w-full before:h-full before:delay-75 before:transition-all before:absolute'>
+          <div class='h-[98%] w-14'>
+            <Image width={30} height={30} class="ml-1 relative left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" src="/folder.png" />
+          </div>
+          <div class='w-[93%]'>
+            <div class='relative top-1/2 -translate-y-1/2 px-2 pt-[1px] font-roboto font-normal text-[24px] truncate'>PlaceHolder</div>
+          </div>
+
+        </div>
       </div>
     </>
   );
