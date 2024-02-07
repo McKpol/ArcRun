cfa7353b4a23069d1e191fb984ad1b6b44293751
McKpol <0809spinespine@gmail.com>
2024-02-07 15:08:57 +0100

    Search Folders, Bug Fixes, Comments in index.tsx

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,15 +14,21 @@ fn cache_programs<>() -> Result<(), Box<dyn std::error::Error>> {
   let path_file = format!("C:/Users/{}/AppData/Roaming/arcrun/", user_name);
   let chpro_file = format!("{}/chpro", path_file);
   let chprocut_file = format!("{}/chprocut", path_file);
+  let chdircut_file = format!("{}/chdircut", path_file);
+  let chdir_file = format!("{}/chdir", path_file);
   
   // Creating dir and file
   fs::create_dir_all(path_file)?;
   let mut file = OpenOptions::new().read(true).write(true).create(true).append(true).open(chpro_file.clone())?;
   let mut filecut = OpenOptions::new().read(true).write(true).create(true).append(true).open(chprocut_file.clone())?;
+  let mut filedircut = OpenOptions::new().read(true).write(true).create(true).append(true).open(chdircut_file.clone())?;
+  let mut filedir = OpenOptions::new().read(true).write(true).create(true).append(true).open(chdir_file.clone())?;
 
   // Empting a file
   fs::write(chpro_file, "")?;
   fs::write(chprocut_file.clone(), "")?;
+  fs::write(chdircut_file.clone(), "")?;
+  fs::write(chdir_file.clone(), "")?;
 
   // Search folder, subfolder and files. Then Write to the file chpro_file, chprocut_file. 
   for programlist in WalkDir::new(program_path).into_iter().filter_map(|file| file.ok()) {
@@ -32,6 +38,14 @@ fn cache_programs<>() -> Result<(), Box<dyn std::error::Error>> {
 
       let path = Path::new(&programlist).file_stem().unwrap().to_str().unwrap();
       writeln!(filecut, "{}", path)?;
+    }
+    if programlist.metadata().unwrap().is_dir(){
+      let programlist = format!("{}", programlist.path().display());
+      writeln!(filedir, "{}", programlist)?;
+
+      let dir = Path::new(&programlist).file_stem().unwrap().to_str().unwrap();
+      writeln!(filedircut, "{}", dir)?;
+
     }
   }
   
@@ -57,9 +71,12 @@ fn search(search: String) -> Vec<String> {
   let user_name = whoami::username();
   let path_file = format!("C:/Users/{}/AppData/Roaming/arcrun/", user_name);
   let chprocut_file = format!("{}/chprocut", path_file);
+  let chdircut_file = format!("{}/chdircut", path_file);
 
   let mut filecut_ = File::open(chprocut_file).unwrap();
+  let mut dircut_ = File::open(chdircut_file).unwrap();
   let mut buf = String::new();
+  let mut bufdir = String::new();
   let mut nline = 0;
   let mut nb_result = 0;
   let mut lista: Vec<String> = Vec::new();
@@ -72,17 +89,37 @@ fn search(search: String) -> Vec<String> {
       // println!("{} {} {}", search, nline, nb_result);
       lista.push(nline.to_string());
       lista.push(buf.lines().nth(nline).unwrap().to_string());
+      lista.push("0".to_string());
+      if nb_result == 5{
+        break;
+      }
+    }
+    nline += 1;
+  }
+
+  dircut_.read_to_string(&mut bufdir).unwrap();
+  nb_result = 0;
+  nline = 0;
+
+  for line in bufdir.lines(){
+    if line.to_lowercase().contains(&search){
+      nb_result += 1;
+      println!("{}", bufdir.lines().nth(nline).unwrap().to_string());
+      lista.push(nline.to_string());
+      lista.push(bufdir.lines().nth(nline).unwrap().to_string());
+      lista.push("1".to_string());
       if nb_result == 3{
         break;
       }
     }
     nline += 1;
   }
-  println!();
-  println!();
-  for element in &lista {
-    println!("{}", element);
-}
+
+//   println!();
+//   println!();
+//   for element in &lista {
+//     println!("{}", element);
+// }
 
   // Sending to FrontEnd
   // let mut prime = "";
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,29 +6,44 @@ import { invoke } from "@tauri-apps/api";
 
 export default component$(() => {
   useVisibleTask$(async () => {
+
+  // Change Size
   await appWindow.setSize(new LogicalSize(825, 90));
-  const result = document.getElementById("result")!;
+  
+  // Change Position
   const x = window.screen.width / 2 - window.innerWidth / 2;
-  const inputElement = document.getElementById("input");
-  const programDiv = document.getElementById("programMain")!;
-  const clone_list: HTMLElement[] = [];
   let y = window.screen.height / 3.8 - window.innerHeight /2;
   await appWindow.setPosition(new LogicalPosition(x, y));
 
+  // Data
+  const result = document.getElementById("result")!;
+  const inputElement = document.getElementById("input");
+  const programDiv = document.getElementById("programMain")!;
+  const dirDiv = document.getElementById("dirMain")!;
+  const clone_list: HTMLElement[] = [];
+
+  // Set Focus, Close when unfocused 
     appWindow.setFocus();
+    await inputElement?.focus();
     await appWindow.onFocusChanged(async () => {
       if (!(await appWindow.isFocused())){
         appWindow.close();
       }
   })
 
+  // When typing do...
   inputElement?.addEventListener("input", async function() {
+
+  // Change position    
     await appWindow.setPosition(new LogicalPosition(x, y));
+  
+  // Searching
     const message: string[] = await invoke("search", {
     search: document.getElementsByTagName("input")[0].value.toLowerCase(),
-  });
-    console.log(message); 
+  
+  }); 
 
+  // Show or Hide if search content is true
     if (message.length == 0){
       result.style.display = "none";
       await appWindow.setSize(new LogicalSize(825, 90));
@@ -38,30 +53,53 @@ export default component$(() => {
       await appWindow.setSize(new LogicalSize(825, 600));
       y = window.screen.height / 2 /* 2 */ - window.innerHeight / 2;
 
+  // Delete all ProgramDiv
       for (let i = 0; clone_list.length > i; i++){
         clone_list[i].remove();
       }
 
-      const programNumber: string[] = [];
-      const programName: string[] = [];
+  // Separation of searchNumber and ProgramName
+      const searchType: string[] = [];
+      const searchNumber: string[] = [];
+      const searchName: string[] = [];
       
-      for (let i = 0; i < message.length; i += 2) {
-        programNumber.push(message[i]);
-        programName.push(message[i + 1]);
+      for (let i = 0; i < message.length; i += 3) {
+        searchNumber.push(message[i]);
+        searchName.push(message[i + 1]);
+        searchType.push(message[i + 2]);
     }
-      programNumber.reverse();
-      programName.reverse();
+  // Reversing list
+      searchNumber.reverse();
+      searchName.reverse();
+      searchType.reverse();
 
+      console.log(searchName);
+
+  // Cloning and filling searchName the ProgramDiv and DirDiv
       for (let i = 0; message.length / 2 > i; i++){
-        console.log(i);
-        const clone = programDiv.cloneNode(true) as HTMLElement;
-        clone.id = i.toString();
-        clone.children[1].children[0].textContent = programName[i].toString();
-        clone.style.display = "flex";
+        const name: any = searchName[i];
         
-        clone_list.push(clone);
-
-        programDiv.parentNode!.insertBefore(clone, programDiv.nextSibling);
+        if (name !== undefined){
+        // ProgramDiv
+          console.log(name);
+          if (searchType[i] == "0"){
+            const clonepro = programDiv.cloneNode(true) as HTMLElement;
+            clonepro.id = i.toString();
+            clonepro.children[1].children[0].textContent = name.toString();
+            clonepro.style.display = "flex";
+            clone_list.push(clonepro);
+            programDiv.parentNode!.insertBefore(clonepro, programDiv.nextSibling);
+          }
+          if (searchType[i] == "1"){
+            // DirDiv
+            const clonedir = dirDiv.cloneNode(true) as HTMLElement;
+            clonedir.id = i.toString();
+            clonedir.children[1].children[0].textContent = name.toString();
+            clonedir.style.display = "flex";
+            clone_list.push(clonedir);
+            dirDiv.parentNode!.insertBefore(clonedir, dirDiv.nextSibling);
+          }
+        }
       }
     }
   });
@@ -97,13 +135,13 @@ export default component$(() => {
         </div>
         
         {/* div jako folder */}
-        <div class='relative shadow-lg left-1/2 -translate-x-1/2 hover:w-full transition-all duration-200 cursor-pointer rounded-[10px] w-[795px] h-[50px] bg-gray-200 flex overflow-hidden 
+        <div id='dirMain' class='hidden mb-1 relative shadow-lg left-1/2 -translate-x-1/2 hover:w-full transition-all duration-200 cursor-pointer rounded-[10px] w-[795px] h-[50px] bg-gray-200 overflow-hidden 
         before:duration-100 before:left-1/2 before:-translate-x-1/2 before:bg-[#2f82f7]/0 before:hover:bg-[#2f82f7] before:w-[96%] before:rounded-[9px] before:hover:w-full before:h-full before:delay-75 before:transition-all before:absolute'>
           <div class='h-[98%] w-14'>
             <Image width={30} height={30} class="ml-1 relative left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" src="/folder.png" />
           </div>
           <div class='w-[93%]'>
-            <div class='relative top-1/2 -translate-y-1/2 px-2 pt-[1px] font-roboto font-normal text-[24px] truncate'>PlaceHolder</div>
+            <div class='relative top-1/2 -translate-y-1/2 px-2 pt-[1px] font-roboto font-normal text-[24px] truncate'></div>
           </div>
 
         </div>
