b4701d1d5d1fcf90336b897439679d19b4625dcc
McKpol <0809spinespine@gmail.com>
2024-03-17 17:58:44 +0100

    Check if is on startup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,11 +8,12 @@ use walkdir::WalkDir;
 use whoami;
 use std::path::Path;
 use std::path::PathBuf;
-use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem, SystemTray, SystemTrayHandle};
+use tauri::{CustomMenuItem, SystemTrayMenu, SystemTray};
 use tauri::SystemTrayEvent;
 use tauri_plugin_autostart::MacosLauncher;
 use std::env;
 use auto_launch::AutoLaunchBuilder;
+use tauri::AppHandle;
 
 
 fn cache_programs<>() -> Result<(), Box<dyn std::error::Error>> {
@@ -60,11 +61,11 @@ fn cache_programs<>() -> Result<(), Box<dyn std::error::Error>> {
   Ok(())
 }
 
-fn SetAutoStart(){
+fn set_auto_start(){
   match env::current_exe() {
     Ok(exe_path) => {
       let auto = AutoLaunchBuilder::new()
-      .set_app_name("the-app")
+      .set_app_name("ArcRun")
       .set_app_path(exe_path.display().to_string().as_str())
       .set_use_launch_agent(true)
       .build()
@@ -79,7 +80,7 @@ fn SetAutoStart(){
       }
     },
     Err(e) => {
-
+      println!("{}", e)
     },
 };
 
@@ -96,7 +97,7 @@ enum TrayState {
   True,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
   println!("Script started");
 
   match cache_programs(){
@@ -104,8 +105,57 @@ fn main() {
     Err(_) => println!("Cannot cached program list")
   }
 
-  let startup = CustomMenuItem::new("startup".to_string(), "Run on startup");
+  // CONFIG FILE (for later)
+
+  // let mut config_fill = ["0"]; // Deafult Settings
+
+  // let user_name = whoami::username();
+  // let config_file = format!("C:/Users/{}/AppData/Roaming/arcrun/config", user_name);
+  // let mut config_open = OpenOptions::new().read(true).write(true).create(true).append(true).open(config_file.clone())?;
+  // let mut config_string = String::new();
+  // config_open.read_to_string(&mut config_string)?;
+
+  // let mut i = 0;
+  // for lines in config_string.lines(){
+  //   i += 1;
+  // }
+
+  // fs::write(config_file.clone(), "")?;
+
+  // for lines in config_fill{
+  //   writeln!(config_open, "{}", lines);
+  // }
+
+  let mut autostart = false;
+
+  match env::current_exe() {
+    Ok(exe_path) => {
+      let auto = AutoLaunchBuilder::new()
+      .set_app_name("ArcRun")
+      .set_app_path(exe_path.display().to_string().as_str())
+      .set_use_launch_agent(true)
+      .build()
+      .unwrap();
+
+      if auto.is_enabled().unwrap() {
+        autostart = true;
+      } else {
+        autostart = false;
+      }
+    },
+    Err(e) => {
+      println!("{}", e);
+    },
+};
+  
+  let startup;
   let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+
+  if autostart {
+    startup = CustomMenuItem::new("startup".to_string(), "Run on startup").selected();
+  } else {
+    startup = CustomMenuItem::new("startup".to_string(), "Run on startup");
+  }
 
   let tray_menu = SystemTrayMenu::new()
   .add_item(startup)
@@ -114,25 +164,29 @@ fn main() {
   let tray = SystemTray::new().with_menu(tray_menu);
 
   tauri::Builder::default()
-  .setup(|app|{
-    app.manage(Mutex::new(TrayState::False));
+  .setup(move |app|{
+    if autostart {
+      app.manage(Mutex::new(TrayState::True));
+    } else {
+      app.manage(Mutex::new(TrayState::False));
+    }
     Ok(())
   })
   .system_tray(tray)
-  .on_system_tray_event(|app, event| match event {
+  .on_system_tray_event(move |app, event| match event {
     SystemTrayEvent::LeftClick {
       position: _,
       size: _,
       ..
     } => {
       let main_window = app.get_window("main").unwrap();
-      main_window.emit("show", "nothing");
+      let _ = main_window.emit("show", "nothing");
     }
     SystemTrayEvent::MenuItemClick { id, .. } => {
       let item_handle = app.tray_handle().get_item(&id);
       match id.as_str() {
           "quit" => {
-          std::process::exit(0);
+          AppHandle::exit(app, 0);
         }
         "startup" => {
           let tray_state_mutex = app.state::<Mutex<TrayState>>();
@@ -140,20 +194,20 @@ fn main() {
           match *tray_state{
             TrayState::True => {
               match env::current_exe() {
-                Ok(e) => {
-                  item_handle.set_selected(false);
+                Ok(_e) => {
+                  let _ = item_handle.set_selected(false);
                   *tray_state = TrayState::False;
-                  SetAutoStart();
+                  set_auto_start();
                 },
                 Err(e) => println!("failed to get current exe path: {e}"),
             };
           }
             TrayState::False => {
               match env::current_exe() {
-                Ok(e) => {
-                  item_handle.set_selected(true);
+                Ok(_e) => {
+                  let _ = item_handle.set_selected(true);
                   *tray_state = TrayState::True;
-                  SetAutoStart();
+                  set_auto_start();
                 },
                 Err(e) => println!("failed to get current exe path: {e}"),
             };
@@ -173,6 +227,7 @@ fn main() {
     .invoke_handler(tauri::generate_handler![search, open])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+  Ok(())
 }
 
 
